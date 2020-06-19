ebbdd2796e781f4bc78cbfc98212785e1423f2ba
Nicolas NGUYEN <ngu.nicolas@gmail.com>
2020-06-20 00:53:13 +0200

    Add translate feature

---
+++ commands/translate.js
@@ -0,0 +1,21 @@
+
+var { translate } = require("google-translate-api-browser");
+
+// TODO : check args
+module.exports = {
+	name: 'translate',
+	description: 'Translate text',
+	async execute(message, args) {
+
+        let from = args[0];
+        let to = args[1];
+        args.splice(0, 2);
+
+        let text = args.join(' ');
+
+        translate(text, {from: from, to:to})
+        .then(res =>{
+            message.reply(text + " > " + res.text)
+        })
+	},
+};
