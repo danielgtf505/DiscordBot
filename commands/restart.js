43e0cc806c2ddb3561a377b507480a714f8508c6
Nicolas NGUYEN <ngu.nicolas@gmail.com>
2020-06-21 13:09:03 +0200

    Add log restarting

--- commands/restart.js
+++ commands/restart.js
@@ -5,6 +5,7 @@ module.exports = {
 	description: 'Random cat picture',
 	async execute(message) {
 		if (message.author.id === OWNER_ID){
+            message.reply(`Restarting ...`)
             process.exit();
         } else {
             message.reply(`You are not owner of the bot.`)
