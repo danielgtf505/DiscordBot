a25316cbb72395b1e5ee0fbe00281e12808509f1
Nicolas NGUYEN <ngu.nicolas@gmail.com>
2020-07-06 23:24:23 +0200

    Add Choose and Random commands

---
+++ commands/random/choose.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ChooseCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'choose',
+			aliases: [],
+			group: 'random',
+			memberName: 'choose',
+            description: 'Let the bot choose for you',
+            args: [
+				{
+					key: 'options',
+					prompt: 'What should i choose from ? (Separated by \';\')',
+					type: 'string',
+                }
+			],
+		});
+    }
+    
+    run(message, {options}) {
+
+        let array = options.split(";");
+        const res = array[Math.floor(Math.random() * array.length)];
+
+		return message.say(`I chose : ${res}`);
+	}
+};
---
+++ commands/random/random.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ChooseCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'random',
+			aliases: ['rand', 'rnd'],
+			group: 'random',
+			memberName: 'random',
+            description: 'Choose a random number',
+            args: [
+				{
+					key: 'lower',
+					prompt: 'Lower bound ?',
+                    type: 'integer',
+                    default: 0
+                }, 
+                {
+					key: 'upper',
+					prompt: 'Upper bound ?',
+                    type: 'integer',
+                    default: 100
+                }
+			],
+		});
+    }
+    
+    run(message, {lower, upper}) {
+
+        if (lower > upper){
+            return message.say(`Lower bound (${lower}) is greater than upper bound (${upper})`);
+        }
+
+        let res = Math.round(Math.random() * (upper - lower) + lower);
+		return message.say(`I chose : ${res}`);
+	}
+};
