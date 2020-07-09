17d1afc1efefca86374e7babbe2e4f4d573b166f
Nicolas NGUYEN <ngu.nicolas@gmail.com>
2020-07-09 20:28:27 +0200

    Add poll command

---
+++ commands/utility/poll.js
@@ -0,0 +1,122 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require("discord.js");
+const { intToEmoji, mapToResult } = require("../../util/util")
+
+const forceEndEmoji = "❌";
+
+module.exports = class PollCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'poll',
+			aliases: [],
+			group: 'utility',
+			memberName: 'poll',
+            description: 'Start a poll.',
+            args: [
+				{
+					key: 'args',
+					prompt: 'Poll options : \` <Question> ; <option 1> ; <options 2> ...; <timeout>\` (max 9 options)',
+					type: 'string',
+                }
+			],
+		});
+    }
+    
+    async run(message, {args}) {
+
+        let array = args.split(";");
+
+        if (array.length < 2 || array.length >= 11){
+            return message.say(`Error in parameters.`);
+        }
+
+        let question = array.shift();
+
+        // Dictionnary containing "emoji - {option - vote }"
+        let results = {};
+        // Map containing {Voter : Option}
+        let voterInfo = new Map();
+
+        let optionText = '';   
+
+        // Fill the result dictionnary and generate text for embededMessage
+        let i = 1;
+        array.forEach(element => {
+            optionText += `${intToEmoji(i)} : \`${element.trim()}\`\n`;
+            results[intToEmoji(i)] = {option: element.trim(), votes: 0};
+            i++;
+        })
+        // Array containing used emojis
+        let usedEmojis = Object.keys(results);
+        usedEmojis.push(forceEndEmoji);
+
+        let embededMessage = new MessageEmbed()
+        .setTitle("Poll")
+        .setAuthor(message.author.username)
+        .setDescription(question)
+        .addField("Options :", optionText)
+        .addField("Controls : ", `${forceEndEmoji} to end poll`)
+        .setColor("#85C1E9")
+        .setTimestamp()
+        .setFooter(); // TODO Put endtime here     
+        var poll = await message.channel.send(embededMessage);
+
+        // Add reactions
+        poll.react(forceEndEmoji);
+        for (let j = 1; j<= array.length; j++){
+            poll.react(intToEmoji(j));
+        }
+
+
+        // collector
+        const filter = (reaction, user) => user.id !== message.client.user.id;
+        var collector = poll.createReactionCollector(filter, {
+            time: 1000000
+        });
+
+        collector.on("collect", (reaction, user) => {
+            if (usedEmojis.includes(reaction.emoji.name)){
+                if (reaction.emoji.name === forceEndEmoji && message.author.id === user.id) return collector.stop();
+
+                if (!voterInfo.has(user.id)) voterInfo.set(user.id, { emoji: reaction.emoji.name });
+
+                const votedEmoji = voterInfo.get(user.id).emoji;
+                if (votedEmoji !== reaction.emoji.name) {
+                    const lastVote = poll.reactions.resolve(votedEmoji);
+                    lastVote.count -= 1;
+                    lastVote.users.remove(user.id);
+                    results[votedEmoji].votes -= 1;
+                    voterInfo.set(user.id, { emoji: reaction.emoji.name });
+                }
+                results[reaction.emoji.name].votes += 1;
+
+            }
+        });
+
+        collector.on('dispose', (reaction, user) => {
+            if (usedEmojis.includes(reaction.emoji.name)) {
+                voterInfo.delete(user.id);
+                results[reaction.emoji.name].votes -= 1;
+            }
+        });
+
+        collector.on('end', () => {
+            let text = '';
+            for (const emoji in results) text += `\`${results[emoji].option}\` - ${results[emoji].votes}\n`;
+            poll.delete();
+
+            let resultEmbed = new MessageEmbed()
+            .setTitle("Poll")
+            .setAuthor(message.author.username)
+            .setDescription(question)
+            .addField("Result : ", text)
+            .setColor("#85C1E9")
+            .setTimestamp()
+            .setFooter(); // TODO Put endtime here    
+
+            message.channel.send(resultEmbed);
+        });
+
+    }
+    
+};
